bc8a215243936308ad78ab210a2fc162f84d17b2
unknown <samuelto1359@gmail.com>
2022-07-15 07:57:10 -0500

    add globos_down

--- javascript/animaciones_winner.js
+++ javascript/animaciones_winner.js
@@ -1,8 +1,13 @@
 var globos_d = document.getElementById("globos_der");
 var globos_i = document.getElementById("globos_izq");
+
+var globos_d_d = document.getElementById("globos_der_d");
+var globos_i_d = document.getElementById("globos_izq_d");
+
 var estrellas = document.getElementById("estrellas");
 var estrellas_container = document.getElementById("tres_estrellas");
 var seccion_g = document.getElementById("section1");
+var seccion_g_d = document.getElementById("globos_down");
 
 function iniciar(){
     
@@ -12,9 +17,19 @@ function iniciar(){
     globos_i.style.paddingTop = '0px';
     globos_i.style.transition = 'all 2s';
 
+    globos_d_d.style.paddingTop = '0px';
+    globos_d_d.style.transition = 'all 2s';
+
+    globos_i_d.style.paddingTop = '0px';
+    globos_i_d.style.transition = 'all 2s';
+
     seccion_g.style.marginLeft = '0%';
     seccion_g.style.marginRight = '0%';
     seccion_g.style.transition = 'all 2s';
+
+    seccion_g_d.style.marginLeft = '0%';
+    seccion_g_d.style.marginRight = '0%';
+    seccion_g_d.style.transition = 'all 2s';
 
     estrellas.style.width = '450px';
     estrellas.style.height = '450px';
