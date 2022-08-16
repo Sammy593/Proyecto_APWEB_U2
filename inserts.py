f1f609826c5a347ae406d0be0d813d9a5b6c5dce
sammy593 <samuelto1359@gmail.com>
2022-08-16 07:57:17 -0500

    inicio sesion

--- inserts.py
+++ inserts.py
@@ -13,6 +13,3 @@ def usuario(pusuario_id, pnombre, papellido, pusuario, pclave, prol_id, pestado)
 def mostrarUsuario():
      for x in conexion.usuarios.find():
       print(x)
-
-usuario("1","Samuel","Ledesma","admin","admin","1","activo")
-usuario("2","Jhostyn","Gavilanez","admin2","admin2","1","activo")
