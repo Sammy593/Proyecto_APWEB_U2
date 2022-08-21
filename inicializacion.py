95563d74f0fd615d0b6c4b42e791f268ffcbd360
SamuelLedesma <wsledesma@espe.edu.ec>
2022-08-21 01:46:24 -0500

    test

--- inicializacion.py
+++ inicializacion.py
@@ -1,15 +1,18 @@
 from flask import Flask
 from flask_mongoengine import MongoEngine
 
-
-app = Flask(__name__, template_folder='templates')
-app.config["MONGODB_SETTINGS"] = [
-{
-"db": "PickOut",
-"host": "localhost",
-"port": 27017,
-"alias": "default",}
-]
-db = MongoEngine()
-db.init_app(app)
-print(db)
+try:
+     app = Flask(__name__, template_folder='templates')
+     app.config["MONGODB_SETTINGS"] = [
+     {
+     "db": "PickOut",
+     "host": "localhost",
+     "port": 27017,
+     "alias": "default",}
+     ]
+     db = MongoEngine()
+     db.init_app(app)
+     print(db)
+except IOError:
+     print(IOError)
+     print("Error de inicializacion")
